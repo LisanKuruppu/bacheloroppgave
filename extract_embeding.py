6ea615ec4e4672d6407bc622373930403717c35b
Lisan Kuruppu <lisankuruppu@gmail.com>
2025-04-02 12:50:35 +0200

    Embeddings

--- extract_embeding.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-embedding_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-
-def get_embedding(text):
-    return embedding_model.encode(text)
